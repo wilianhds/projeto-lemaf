a9f2a5d0fcb8363408f0c3970078c64e6168a599
wilianhds <whs@estudante.ufla.br>
2020-05-23 12:28:07 -0300

    Backend adicionadoao projeto

---
+++ backend/src/main/java/br/com/projeto/zetta/ZettaApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.zetta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZettaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZettaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/projeto/zetta/ZettaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.zetta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZettaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
